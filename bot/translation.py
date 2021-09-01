2a148685aa59c6f586ac79a7b946a0b6dec0c1b7
Iamgoingtomake <89531655+Iamgoingtomake@users.noreply.github.com>
2021-09-01 11:24:09 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -61,7 +61,7 @@ Maintained By: @Hyetelegram
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href=https://t.me/Hyetelegram</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
